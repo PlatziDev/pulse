90c243b3e335bc9419c9bb385de8d75742235c71
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-21 19:48:04 -0500

    [add] next configuration file

---
+++ renderer/next.config.js
@@ -0,0 +1,24 @@
+const BabiliPlugin = require('babili-webpack-plugin')
+
+module.exports = {
+  webpack(config, { dev }) {
+    config.target = 'electron-renderer'
+
+    config.plugins = config.plugins.filter(plugin => {
+      return plugin.constructor.name !== 'UglifyJsPlugin'
+    })
+
+    if (!dev) {
+      config.plugins.push(new BabiliPlugin())
+    }
+
+    return config
+  },
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+      '/editor': { page: '/editor' },
+    }
+  },
+  assetPrefix: './',
+}

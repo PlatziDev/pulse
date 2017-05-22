e54a157aa8cd56762188894ee14c89e679483997
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-21 19:43:44 -0500

    [add] custom document

---
+++ renderer/pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import flush from 'styled-jsx/server'
+
+export default class extends Document {
+  static getInitialProps({ renderPage }) {
+    const { html, head, errorHtml, chunks } = renderPage()
+    const styles = flush()
+    return { html, head, errorHtml, chunks, styles }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>Pulse</title>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          />
+          <style>{`body { margin: 0;font-family: sans-serif; }`}</style>
+        </Head>
+        <body className="custom_class">
+          {this.props.customValue}
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}

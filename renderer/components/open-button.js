dc72dc19a19f95dde1424e91dfd96b04ef283a2d
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-21 19:43:30 -0500

    [add] open file button component

---
+++ renderer/components/open-button.js
@@ -0,0 +1,54 @@
+import { Component } from 'react'
+import { Base } from 'pulse-editor/buttons'
+import { ipcRenderer } from 'electron'
+import { string, func } from 'prop-types'
+import isMac from 'pulse-editor/built/utils/is-mac'
+
+import Icon from './icon'
+
+export default class OpenButton extends Component {
+  static contextTypes = {
+    setShortcut: func.isRequired,
+    removeShortcut: func.isRequired,
+    writeValue: func.isRequired,
+    setFileName: func.isRequired,
+  }
+
+  componentDidMount() {
+    ipcRenderer.on('file-opened', this.handleOpen)
+    this.context.setShortcut({
+      ctrlKey: !isMac(),
+      metaKey: isMac(),
+      altKey: false,
+      shiftKey: false,
+      keyName: 'o',
+      updater: selected => selected,
+      handler: event => {
+        this.openFile()
+        return event.selection
+      },
+    })
+  }
+
+  componentWillUnmount() {
+    this.context.removeShortcut({ keyName: 'o' })
+    ipcRenderer.removeListener('file-opened', this.handleOpen)
+  }
+
+  openFile = () => ipcRenderer.send('open-file')
+
+  handleClick = () => this.openFile()
+
+  handleOpen = (event, fileName, content) => {
+    this.context.setFileName(fileName)
+    this.context.writeValue({ target: { value: content } })
+  }
+
+  render = () => (
+    <Base onClick={this.handleClick} name="open">
+      <span>
+        <Icon name="folder-open-o" title="Open file [CMD+O]" /> Open file
+      </span>
+    </Base>
+  )
+}
